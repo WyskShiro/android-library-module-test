f9a6b955e72134b684a0accb5bf26594dfed02f4
William <william@jera.com.br>
2020-04-22 17:12:14 -0400

    Method for getting my spectacular activity

--- validatetor/src/main/java/will/shiro/validatetor/Extensions.kt
+++ validatetor/src/main/java/will/shiro/validatetor/Extensions.kt
@@ -1,5 +1,12 @@
 package will.shiro.validatetor
 
+import android.content.Context
+import android.content.Intent
+
 fun return5(): Int {
     return 5
+}
+
+fun getMySpectacularActivity(context: Context): Intent {
+    return Intent(context, MainActivity::class.java)
 }
